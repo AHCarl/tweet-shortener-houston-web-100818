5cd752480d7326387d2126a37eca7f6afaaaf599
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 02:39:14 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@ dictionary = {"hello" => 'hi', "to, two, too" => '2', "for, four" => '4', "be" =
   "at" => '@', "and" => '&'}
   
 def word_substituter(tweets)
-  tweets.each do |tweet|
+  tweets.split(" ").each do |tweet|
     tweet.gsub!(/ two | too | to /, " 2 ")
     tweet.gsub!(/four | for /, " 4 ")
     tweet.gsub!(" be ", " b ")
