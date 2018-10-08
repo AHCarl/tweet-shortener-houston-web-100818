8b4294097e23bbd12f61fb59d65bab80747e7af6
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 03:16:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@ def word_substituter(tweet)
   dictionary = { hello: "hi",to: "2",two: "2",too: "2",for: "4",four: "4",be: "b",you: "u",
   at: "@",and: "&"}
   
-  tweet.split(" ").map{ |t| dictionary.key?(t.downcase.to_sym)? dictionary[t.downcase.to_sym] : 
+  tweet.split(" ").map{ |t| dictionary.key?(t.downcase.to_sym) ? dictionary[t.downcase.to_sym] : 
   t}.join(" ")
 end
 
@@ -17,4 +17,7 @@ def selective_tweet_shortener(tweet)
   tweet.length > 140 ? word_substituter(tweet) : tweet 
 end
 
-def shortened_tweet_truncator()
+def shortened_tweet_truncator(tweet)
+  t = word_substituter(tweet)
+  t.length > 140 ? "#{t[0..136]}..." : t 
+end 
