9b15b5812cf62c2e0248891070fbfdbde8abadd6
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 02:46:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,6 +2,13 @@ dictionary = {"hello" => 'hi', "to, two, too" => '2', "for, four" => '4', "be" =
   "at" => '@', "and" => '&'}
   
 def word_substituter(tweet, dictionary)
-  tweets.split(" ").each do |tweet|
-    if tweet.any
+  current_word = ""
+  shortened_tweet = ""
+  
+  str.each_char do |char|
+    shortened_word = (dictionary[current_word] || current_word)
+    shortened_tweet += shortened_word + char 
+    current_word = ""
+  end
+  shortened_tweet
 end
