a3c7ed51539372a11044f4376c2fc42b2dfac965
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 01:58:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,5 @@
-def dictionary
-  {"hello" => 'hi', "to, two, too" => '2', "for, four" => '4', "be" => 'b', "you" => 'u',
+dictionary = {"hello" => 'hi', "to, two, too" => '2', "for, four" => '4', "be" => 'b', "you" => 'u',
   "at" => '@', "and" => '&'}
-end
   
 def word_substituter(tweet)
   tweet.split(" ")
