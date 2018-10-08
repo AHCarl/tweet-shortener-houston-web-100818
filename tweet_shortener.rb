cb30dfcb111a1ff4956df7b2ff36596d7a30472e
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 03:10:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,8 @@
 def word_substituter(tweet)
-  dictionary = { hello: "hi",
-    to: "2",
-    two: "2",
-    too: "2",
-    for: "4",
-    four: "4",
-    be: "b",
-    you: "u",
-    at: "@",
-    and: "&"
-  }
+  dictionary = { hello: "hi",to: "2",two: "2",too: "2",for: "4",four: "4",be: "b",you: "u",
+  at: "@",and: "&"}
+  
   tweet.split(" ").map{ |t| dictionary.key?(t.downcase.to_sym)? dictionary[t.downcase.to_sym] : 
   t}.join(" ")
 end
+
